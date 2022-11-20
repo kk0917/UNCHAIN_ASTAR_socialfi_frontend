0aad612ae015e53e111b67d314586710a268c4bc
SHØ <neila@users.noreply.github.com>
2022-11-20 17:10:20 +0900

    base config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
+  images: {
+    domains: ['images.unsplash.com', 'plus.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
